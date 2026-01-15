80ebefc1ff58d67f5d980061ce7a20fbd8cb595a
Michael Sintim <michaelsintim673@gmail.com>
2026-01-15 00:58:56 +0000

    Updates

--- hello.js
+++ hello.js
@@ -1,2 +1,44 @@
-console.log('hello world ')
-alert('hey')
+// console.log('hello world ')
+// // alert('hey')
+
+// let $function = 'clock it '
+
+// const country = 'Ghana';
+// const continent = 'Africa';
+// const population = (8000000000 / 2)+1
+// const isIsland = false ; 
+// let language;
+// const description =  country + 'is in ' + continent
+// language = 'english'
+
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+
+// console.log(typeof country);
+// console.log(typeof continent);
+// console.log(typeof isIsland);
+// console.log(typeof population);
+// console.log(typeof language);
+
+// if (population > 6000000)console.log('Ghana has a greater population')
+   
+// console.log(description)
+
+const john_mass = 92 ;
+const john_height = 1.95;
+
+const johnBMI = john_mass/ john_height**2
+
+
+const mark_mass = 78 ;
+const mark_height = 1.69;
+
+const markBMI = (mark_mass)/ mark_height**2
+// console.log(markBMI,johnBMI,markHigherBMI)
+
+markHigherBMI  = johnBMI > markBMI
+
+console.log(markBMI,johnBMI,markHigherBMI)
