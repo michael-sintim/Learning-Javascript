a2e4cadc308add6aa61845e3f078ec582d18b5ca
Michael Sintim <michaelsintim673@gmail.com>
2026-01-27 16:15:59 +0000

    Pig game project

--- modal/script.js
+++ modal/script.js
@@ -4,12 +4,35 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal')
 const btnOpenModal = document.querySelectorAll('.show-modal')
-console.log(btnOpenModal)
+
+const closemodal = function(){
+    console.log('Button clicked');
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+const OpenMoodal =  function(){
+            console.log("Button clicked")
+            modal.classList.remove('hidden')
+            overlay.classList.remove('hidden')
+
+}
 
 for  (let i = 0; i < btnOpenModal.length; i++){
-    console.log(btnOpenModal[i].textContent('click',
-        function(){
-            
-        }
-    ));
+    console.log(btnOpenModal[i].addEventListener('click', OpenMoodal))
+
 }
+
+
+btnCloseModal.addEventListener('click', closemodal)
+overlay.addEventListener('click', closemodal)
+
+
+document.addEventListener('keydown', (e) => {
+console.log(e.key)
+
+if (e.key==='Escape' && !modal.classList.contains('hidden'))
+{
+        closemodal();
+    }
+})
