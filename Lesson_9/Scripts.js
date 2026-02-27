5da7a9b076ab62f6f0c54a14aacf9b7753d79b9e
Michael Sintim <michaelsintim673@gmail.com>
2026-02-27 14:42:56 +0000

    Data structures

--- Lesson_9/Scripts.js
+++ Lesson_9/Scripts.js
@@ -1,8 +1,8 @@
 
 const arrs = [1,2,3];
 
-let [x,y,z] = arrs;
-console.log(x,y,z); 
+
+let [x,y,z] = arrs
 
 [x,y,z] = [z,x,y]
 console.log(x,y,z)
@@ -31,9 +31,9 @@ console.log(k,j,w)
 // const ratingStars = [63405, 1808];
 // const [fiveStarRatings,oneStarRatings,threeStarRatings=0 ] = ratingStars;
 
+//const {title,author,isbn} = book 
 
 
-// 
 
 const restaurant = {
   name: "Skyline Bistro",
@@ -55,15 +55,15 @@ const restaurant = {
   ],
 
   openingHours: {
-    monday: {
+    mon: {
       open: "08:00",
       close: "22:00"
     },
-    wednesday: {
+    wed: {
       open: "09:00",
       close: "23:00"
     },
-    friday: {
+    fri: {
       open: "10:00",
       close: "01:00"
     }
@@ -78,3 +78,74 @@ const restaurant = {
 
 const {name,openingHours,categories} = restaurant
 console.log(name,openingHours,categories)
+
+const arr = [1,3]
+const newArr = [1,2,...arr]  // the spread operator
+const rest = [...restaurant.mainMenu,'chicken']
+
+const menu = [...restaurant.mainMenu,...restaurant.starterMenu]
+console.log(menu)
+
+//for off loop
+for (const item of menu) console.log(item)
+
+for (const item of menu.entries()){
+  console.log(item)
+}
+
+console.log(...menu.entries())
+
+// iterables arrays strings maps and sets and not object
+
+const news = 'michael'
+const letters = [...news]
+console.log(letters)
+// used only when using arrays or pass a value into  a function
+
+
+const ax = [1,3,...[31,32]]
+const [ab,ba,...others] = [11,12,13,15
+]
+console.log(ab,ba,others)
+
+const r1 = {
+  name: 'Capri',
+  numGUests: 20,
+}
+
+const r2 = {
+  name: 'Capri',
+  owner: 'me Boi',
+}
+
+r1.numGUests ||= 10;
+r1.numGUests ||= 10;
+
+r1.numGUests &&= 'treason';
+r2.numGUests &&= 'treason';
+
+r1.numGUests ??= 10;
+r2.numGUests ??= 10;
+
+
+const days = ['mon','tue','wed','thu','fri','sat','sun'];
+
+for(const day of days){
+  const open = restaurant.openingHours[day]?.open || 'closed'
+  console.log(`on ${day}, we open at ${open}`);
+  
+}
+
+//objects keys
+const properties = Object.keys(openingHours)
+let  openstr =`\nwe are open ${properties.length} days:  `
+
+for (const daye of properties){
+  openstr += `${daye}, `;
+}
+console.log(openstr)
+
+//sets
+//.size not .length  ,.add ,.delete 
+//sets is used to remove duplicate values 
+const n_set = new Set(['Pasta','pizaa','Pasta','pizaa'])
